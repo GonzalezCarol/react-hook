c046f019198fd0a21f09c33841ca621ec0e4d348
carol <carolina.gonzalez.alvarenga@gmail.com>
2021-03-10 15:55:01 -0300

    usestate hook

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -4,6 +4,7 @@ import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseState = (props) => {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
   return (
     <div className="UseState">
@@ -15,10 +16,21 @@ const UseState = (props) => {
       <div className="center">
         <span className="text">{count}</span>
         <div>
-          <button className="btn" onClick={() => setCount(count -1 ) }>-1</button>
-          <button className="btn" onClick={() => setCount(count +1 ) }>+1</button>
+          <button className="btn" onClick={() => setCount(count - 1)}>
+            -1
+          </button>
+          <button className="btn" onClick={() => setCount(count + 1)}>
+            +1
+          </button>
         </div>
       </div>
+      <SectionTitle title="Exerício #02" />
+      <input
+        type="text"
+        className="input"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
     </div>
   );
 };
