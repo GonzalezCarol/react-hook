befee7f78d3a38d32cd29447f036f7f32e0b4697
carol <carolina.gonzalez.alvarenga@gmail.com>
2021-03-10 21:29:49 -0300

    useefec hook challenge

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,15 +1,78 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState, useEffect } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
-const UseEffect = (props) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
+function calculateFatorial(num) {
+  const number = parseInt(num);
+  if (number < 0) return -1;
+  if (number === 0) return 1;
+  return calculateFatorial(number - 1) * number;
 }
 
-export default UseEffect
+const UseEffect = (props) => {
+  const [number, setNumber] = useState(1);
+  const [fatorial, setFatorial] = useState(1);
+  const [oddOrEvenNumber, setOddOrEvenNumber] = useState(1);
+  const [oddOrEvenStatus, setOddOrEvenStatus] = useState("");
+
+  useEffect(
+    function () {
+      setFatorial(calculateFatorial(number));
+    },
+    [number]
+  );
+
+  useEffect(
+    function () {
+      if (fatorial > 10000) {
+        document.title = "eita!";
+      }
+    },
+    [fatorial]
+  );
+
+  useEffect(function () {
+    setOddOrEvenStatus(oddOrEvenNumber % 2 === 0 ? "Par" : "Impar")
+    
+  }, [oddOrEvenNumber]);
+
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+      <SectionTitle title="Exercicio #01" />
+
+      <div className="center">
+        <div>
+          <span className="text">Fatorial:</span>
+          <span className="text red">
+            {fatorial === -1 ? "não existe" : fatorial}
+          </span>
+        </div>
+        <input
+          type="number"
+          className="input"
+          value={number}
+          onChange={(event) => setNumber(event.target.value)}
+        />
+      </div>
+      <SectionTitle title="Exercicio #01" />
+      <div className="center">
+        <span className="text">
+          Qual o tipo?
+          
+        </span>
+        <span className="text read">{oddOrEvenStatus}</span>
+        <input
+          type="number"
+          className="input"
+          onChange={(event) => setOddOrEvenNumber(event.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UseEffect;
