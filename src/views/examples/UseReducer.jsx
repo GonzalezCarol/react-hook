9df34292d21f8df326a9fd8ae6323103f0b9f8ea
carol <carolina.gonzalez.alvarenga@gmail.com>
2021-03-18 22:42:31 -0300

    usereducer challenge

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -13,7 +13,14 @@ function reducer(state, action) {
   switch (action.type) {
     case "numberAdd2":
       return { ...state, number: state.number + 2 };
-
+    case "numberMult7":
+      return { ...state, number: state.number * 7 };
+    case "numberDiv25":
+      return { ...state, number: state.number / 25 };
+    case "numberInt":
+      return { ...state, number: parseInt(state.number)};
+    case "numberAddN":
+      return { ...state, number: state.number + action.payLoad};
     case "login":
       return { ...state, user: { name: action.name } };
     default:
@@ -39,6 +46,11 @@ const UseReducer = (props) => {
         <span className="text">{state.number}</span>
         <div>
           <button className="btn" onClick={()=> dispatch({ type: "login", name:'marina' })}> Login </button>
+          <button className="btn" onClick={()=> dispatch({ type: "numberMult7"})}> * 7 </button>
+          <button className="btn" onClick={()=> dispatch({ type: "numberDiv25"})}> / 25 </button>
+          <button className="btn" onClick={()=> dispatch({ type: "numberInt"})}> Int </button>
+          <button className="btn" onClick={()=> dispatch({ type: "numberAddN", payLoad: -9})}> - 9 </button>
+          <button className="btn" onClick={()=> dispatch({ type: "numberAddN", payLoad: +9})}> + 9 </button>
           <button className="btn" onClick={() => dispatch({ type: "numberAdd2" })}> +2 </button>  {/* dentro do dispatch o objeto é uma ação, entao eu passo o type que é o numberAdd2*/}
         </div>
       </div>
